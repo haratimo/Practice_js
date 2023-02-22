87666c21bb0b39643c2a57f45d48f7320d39309a
haratimo <104927301+haratimo@users.noreply.github.com>
2023-02-22 16:18:29 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
-// Read CSV file and define stimulus array
-const stimuli = [];
-$.ajax({
-  url: "../stimuli.csv",
-  async: false,
-  success: function (csvd) {
-    const rows = Papa.parse(csvd).data;
-    for (let i = 0; i < rows.length; i++) {
-      stimuli.push(rows[i][0]);
-    }
-  },
+import * as jsPsychModule from "../jsPsych/jspsych.js";
+
+// Define stimulus array
+let stimuli = [];
+
+// Load CSV file
+jsPsychModule.pluginAPI.loadCSV("stimuli.csv", function(data){
+  stimuli = data.map(function(row){
+    return row.stimulus;
+  });
 });
 
 // Shuffle stimuli and divide into sets of four
-const shuffledStimuli = jsPsych.randomization.shuffle(stimuli);
-const stimulusSets = jsPsych.utils.chunkArray(shuffledStimuli, 4);
+const shuffledStimuli = jsPsychModule.randomization.shuffle(stimuli);
+const stimulusSets = jsPsychModule.utils.chunkArray(shuffledStimuli, 4);
 
 // Define maxdiff plugin trial
 const maxDiffTrial = {
@@ -26,11 +25,24 @@ const maxDiffTrial = {
   button_html: '<button class="maxdiff-option jspsych-btn">%choice%</button>',
   response_ends_trial: true,
   on_load: function () {
-    jsPsych.pluginAPI.clearAllTimeouts();
+    jsPsychModule.pluginAPI.clearAllTimeouts();
   },
   on_finish: function (data) {
     console.log(data);
   },
 };
 
-// Welcome
+// Welcome trial
+const welcomeTrial = {
+  type: "html-keyboard-response",
+  stimulus: "<p>Welcome to the experiment!</p><p>Press any key to begin.</p>",
+  post_trial_gap: 1000,
+};
+
+// Experiment timeline
+const timeline = [welcomeTrial, maxDiffTrial];
+
+// Run experiment
+jsPsychModule.init({
+  timeline: timeline,
+});
