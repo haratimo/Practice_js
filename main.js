5825f4795a3b47b773b4bdd4e817c5ebfff7ef13
haratimo <104927301+haratimo@users.noreply.github.com>
2023-02-22 23:07:23 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,58 +1,66 @@
 import * as jsPsychModule from "https://unpkg.com/jspsych@7.3.1/jspsych.js";
 
-// Define stimulus array
-const stimuli = [
-  "Stimulus 1",
-  "Stimulus 2",
-  "Stimulus 3",
-  "Stimulus 4",
-  "Stimulus 5",
-  "Stimulus 6",
-  "Stimulus 7",
-  "Stimulus 8",
-  "Stimulus 9",
-  "Stimulus 10",
-  "Stimulus 11",
-  "Stimulus 12",
-  "Stimulus 13",
-  "Stimulus 14",
-  "Stimulus 15",
-  "Stimulus 16"
-];
+const timeline = [];
 
-// Shuffle stimuli and divide into sets of four
-const shuffledStimuli = jsPsychModule.randomization.shuffle(stimuli);
-const stimulusSets = jsPsychModule.utils.chunkArray(shuffledStimuli, 4);
-
-// Define maxdiff plugin trial
-const maxDiffTrial = {
-  type: "maxdiff",
-  prompt: "<p>Choose the best and worst metaphor:</p>",
-  labels: ["Best", "Worst"],
-  options: stimulusSets,
-  repetitions: 8,
-  require_selection: [1, 1],
-  button_html: '<button class="maxdiff-option jspsych-btn">%choice%</button>',
-  response_ends_trial: true,
-  on_load: function () {
-    jsPsychModule.pluginAPI.clearAllTimeouts();
-  },
-  on_finish: function (data) {
-    console.log(data);
-  },
-};
-
-// Welcome trial
 const welcomeTrial = {
   type: "html-keyboard-response",
   stimulus: "<p>Welcome to the experiment!</p><p>Press any key to begin.</p>",
-  post_trial_gap: 1000,
+  post_trial_gap: 1000
 };
 
-// Experiment timeline
-const timeline = [welcomeTrial, maxDiffTrial];
+const instructionsTrial = {
+  type: "html-keyboard-response",
+  stimulus: "<p>Instructions:</p><p>In this experiment, you will be presented with a series of pairs of metaphors. For each pair, you will be asked to choose the one that is best and the one that is worst. Please try to make your choices based on your gut feeling, rather than thinking too hard about it. </p><p>Press any key to begin.</p>",
+  post_trial_gap: 1000
+};
 
-// Run experiment
-jsPsychModule.init({
+const stimuli = [
+  "The snow was a white blanket",
+  "He had a voice as loud as a lion",
+  "The dancer was a graceful swan",
+  "She was a cheetah on the track",
+  "The sun was a ball of fire",
+  "The computer was an old dinosaur",
+  "The wind was a howling wolf",
+  "His words were music to my ears",
+  "The leaves were dancers in the wind",
+  "The engine was a pounding heart",
+  "The waves were soldiers marching to shore",
+  "The coffee was a warm blanket",
+  "Her eyes were gleaming diamonds",
+  "The tree was a tower touching the sky",
+  "The river was a snake slithering through the valley",
+  "The night was a black velvet cloak"
+];
+
+const shuffledStimuli = jsPsych.randomization.shuffle(stimuli);
+const stimulusSets = jsPsych.utils.chunkArray(shuffledStimuli, 2);
+
+for (let i = 0; i < stimulusSets.length; i++) {
+  const maxDiffTrial = {
+    type: "maxdiff",
+    prompt: "<p>Choose the best and worst metaphor:</p>",
+    labels: ["Best", "Worst"],
+    options: stimulusSets[i],
+    require_selection: [1, 1],
+    button_html: '<button class="maxdiff-option jspsych-btn">%choice%</button>',
+    response_ends_trial: true
+  };
+
+  timeline.push(maxDiffTrial);
+}
+
+const debriefTrial = {
+  type: "html-keyboard-response",
+  stimulus: "<p>Thank you for participating in this experiment!</p><p>Press any key to exit.</p>",
+  post_trial_gap: 1000
+};
+
+timeline.unshift(debriefTrial);
+timeline.unshift(instructionsTrial);
+timeline.unshift(welcomeTrial);
+
+jsPsych.init({
   timeline: timeline,
+  show_progress_bar: true
 });
