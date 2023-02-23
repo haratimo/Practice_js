627e6554a124db8631310d4dd94200f4cd7623dd
haratimo <104927301+haratimo@users.noreply.github.com>
2023-02-22 17:36:43 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -6,44 +6,41 @@ jsPsychModule.pluginAPI.loadCSV("stimuli.csv", function(data){
   stimuli = data.map(function(row){
     return row.stimulus;
   });
-});
 
-// Shuffle stimuli and divide into sets of four
-const shuffledStimuli = jsPsychModule.randomization.shuffle(stimuli);
-const stimulusSets = jsPsychModule.utils.chunkArray(shuffledStimuli, 4);
+  // Shuffle stimuli and divide into sets of four
+  const shuffledStimuli = jsPsychModule.randomization.shuffle(stimuli);
+  const stimulusSets = jsPsychModule.utils.chunkArray(shuffledStimuli, 4);
 
-// Define maxdiff plugin trial
-const maxDiffTrial = {
-  type: "maxdiff",
-  prompt: "<p>Choose the best and worst metaphor:</p>",
-  labels: ["Best", "Worst"],
-  options: stimulusSets,
-  repetitions: 8,
-  require_selection: [1, 1],
-  button_html: '<button class="maxdiff-option jspsych-btn">%choice%</button>',
-  response_ends_trial: true,
-  on_load: function () {
-    jsPsychModule.pluginAPI.clearAllTimeouts();
-  },
-  on_finish: function (data) {
-    console.log(data);
-  },
-};
+  // Define maxdiff plugin trial
+  const maxDiffTrial = {
+    type: "maxdiff",
+    prompt: "<p>Choose the best and worst metaphor:</p>",
+    labels: ["Best", "Worst"],
+    options: stimulusSets,
+    repetitions: 8,
+    require_selection: [1, 1],
+    button_html: '<button class="maxdiff-option jspsych-btn">%choice%</button>',
+    response_ends_trial: true,
+    on_load: function () {
+      jsPsychModule.pluginAPI.clearAllTimeouts();
+    },
+    on_finish: function (data) {
+      console.log(data);
+    },
+  };
 
-// Welcome trial
-const welcomeTrial = {
-  type: "html-keyboard-response",
-  stimulus: "<p>Welcome to the experiment!</p><p>Press any key to begin.</p>",
-  post_trial_gap: 1000,
-};
+  // Welcome trial
+  const welcomeTrial = {
+    type: "html-keyboard-response",
+    stimulus: "<p>Welcome to the experiment!</p><p>Press any key to begin.</p>",
+    post_trial_gap: 1000,
+  };
 
-// Experiment timeline
-const timeline = [welcomeTrial, maxDiffTrial];
+  // Experiment timeline
+  const timeline = [welcomeTrial, maxDiffTrial];
 
-// Run experiment
-jsPsychModule.init({
-  timeline: timeline,
-  on_finish: function() {
-    jsPsychModule.data.displayData();
-  }
+  // Run experiment
+  jsPsychModule.init({
+    timeline: timeline,
+  });
 });
